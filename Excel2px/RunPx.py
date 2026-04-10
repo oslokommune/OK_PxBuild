410aa7fd61a69fadca968afae5966fbd643f7d08
Øystein Haga <oystein.haga@byr.oslo.kommune.no>
2026-04-10 14:21:09 +0200

    Remove Excel2px folder from git tracking and add to .gitignore

--- Excel2px/RunPx.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-import pxbuild
-
-ID = input("Enter ID of the dataset: ")
-Path(f"Excel2px/output/px/output_{ID}").mkdir(parents=True, exist_ok=True)
-pxbuild.LoadFromPxmetadata(ID, "Excel2px/pxjson/pxbuildconfig/my_config.json")

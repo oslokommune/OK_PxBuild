fde31238af268d114a447ba37133edc5faec2462
Øystein Haga <oystein.haga@byr.oslo.kommune.no>
2026-05-27 10:10:37 +0200

    Removed folders, keeping only csv2px and pxbuild

--- tests/controll/test_cube_1.py
+++
@@ -1,6 +0,0 @@
-import pxbuild
-
-
-class TestCube1:
-    def test_cube_1_dont_crash(self):
-        pxbuild.LoadFromPxmetadata("1", "testdata/test_cube_1/test_config.json")
--- tests/operations_on_model/output/refine/test_apply_default_language.py
+++
@@ -1,8 +0,0 @@
-from pxbuild.controll.load_from_pxfile import Loader
-from pxbuild.operations_on_model.output.refine.apply_default_language import apply_default_language
-
-
-def test_apply_default_language_ok():
-    dummy = Loader("testdata/clean_me.px")
-    apply_default_language(dummy.outModel)
-    assert dummy.outModel.subject_area.has_value("fi")
